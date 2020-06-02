8e00007ad5963d5312d93e3d1ef86d3aadb6234c
Simon <simonsch.tz@gmail.com>
2020-06-02 17:24:15 +0200

    Initialize all needed files for the blog

---
+++ client/src/blog/Blog.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default function Blog() {
+
+        return (
+            <div>
+                <h2>blog.</h2>
+            </div>
+        );
+    
+}
---
+++ client/src/blog/BlogInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default function BlogInput() {
+
+        return (
+            <div>
+                <h2>blog.</h2>
+            </div>
+        );
+    
+}
