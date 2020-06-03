527d29831e2e03c918a48313ba8457dd055dc314
Simon <simonsch.tz@gmail.com>
2020-06-03 18:00:17 +0200

    Initialize Schedule.js

---
+++ client/src/schedule/Schedule.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Schedule() {
+    return (
+        <div className="not-stream-component">
+            <h2>schedule</h2>
+        </div>
+    )
+}
