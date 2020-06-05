260365247e6f4a2576a86e876eec80d3d66c00b0
fran <francisco.chiarino@gmail.com>
2020-06-05 15:59:09 +0200

    Only Admin can post, put or delete archive

--- routes/archiveRoute.js
+++ routes/archiveRoute.js
@@ -4,11 +4,11 @@ const auth = require("../middleware/tokenAuthenticator");
 const isAdmin = require("../middleware/rolesAuthenticator");
 
 Route.get("/", auth, getArchive);
-Route.post("/post", auth, postArchive); //isAdmin
+Route.post("/post", auth, isAdmin, postArchive); //isAdmin
 //Alternative syntax for practice purposes
 Route.route("/:id")
     .get(auth, getArchiveById)
-    .put(auth, putArchive)//isAdmin
-    .delete(auth, deleteArchive);//isAdmin
+    .put(auth, isAdmin, putArchive)//isAdmin
+    .delete(auth, isAdmin, deleteArchive);//isAdmin
 
 module.exports = Route;
