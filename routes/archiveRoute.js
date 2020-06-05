ca09ab9fef95ddf9dbd1c861c89861b44bbfac70
fran <francisco.chiarino@gmail.com>
2020-06-05 16:21:11 +0200

    Authentication and isAdmin functions work as expected.

--- routes/archiveRoute.js
+++ routes/archiveRoute.js
@@ -4,11 +4,11 @@ const auth = require("../middleware/tokenAuthenticator");
 const isAdmin = require("../middleware/rolesAuthenticator");
 
 Route.get("/", auth, getArchive);
-Route.post("/post", auth, isAdmin, postArchive); //isAdmin
+Route.post("/post", auth, isAdmin, postArchive); 
 //Alternative syntax for practice purposes
 Route.route("/:id")
     .get(auth, getArchiveById)
-    .put(auth, isAdmin, putArchive)//isAdmin
-    .delete(auth, isAdmin, deleteArchive);//isAdmin
+    .put(auth, isAdmin, putArchive)
+    .delete(auth, isAdmin, deleteArchive);
 
 module.exports = Route;
--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -4,11 +4,11 @@ const auth = require("../middleware/tokenAuthenticator");
 const isAdmin = require("../middleware/rolesAuthenticator");
 
 Route.get("/", auth, getBlog);
-Route.post("/post", auth, isAdmin, postBlog); //isAdmin
+Route.post("/post", auth, isAdmin, postBlog); 
 //Alternative syntax for practice purposes
 Route.route("/:id")
     .get(auth, getBlogById)
-    .put(auth, isAdmin, putBlog)//isAdmin
-    .delete(auth, isAdmin, deleteBlog);//isAdmin
+    .put(auth, isAdmin, putBlog)
+    .delete(auth, isAdmin, deleteBlog);
 
 module.exports = Route;
