ebbcc6d9d4235705327ff579bfb1b4734927ecdc
fran <francisco.chiarino@gmail.com>
2020-06-07 13:15:57 +0200

    Login and logout seem to be working fine. Will polish some stuff on back-end

--- client/src/CreateUser.js
+++ client/src/CreateUser.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import {Redirect} from 'react-router-dom';
 
 export default function CreateUser(props) {
     const [firstName, setFirstName] = useState("");
