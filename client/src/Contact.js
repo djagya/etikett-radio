5c7153f7e87a770c1395e2a955e8411da64776f7
fran <francisco.chiarino@gmail.com>
2020-06-08 10:35:49 +0200

    Almost done with front-end

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -6,21 +6,25 @@ export default function Contact() {
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
 
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
     return (
         <div className="not-stream-component">
             <h1>Contact</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="name">
                     <span className="required">*</span>name
-                <input type="text" id="name" placeholder="Name" value={name} />
+                <input type="text" id="name" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
                 </label>
                 <label htmlFor="subject">
                     <span className="required">*</span>subject
-                <input type="text" id="subject" placeholder="Subject" value={subject} />
+                <input type="text" id="subject" placeholder="Subject" value={subject} onChange={(e) => setSubject(e.target.value)} />
                 </label>
                 <label htmlFor="message">
                     <span className="required">*</span>message
-                <input type="textarea" id="message" placeholder="Message" value={message} />
+                <input type="textarea" id="message" placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} />
                 </label>
                 <button type="submit">Send</button>
             </form>
