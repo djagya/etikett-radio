2422dca7a2d7f59c22929e6f3ba4a3d1bc3b5502
fran <francisco.chiarino@gmail.com>
2020-06-09 20:47:59 +0200

    Encountered some bugs

--- client/src/chat/Message.js
+++ client/src/chat/Message.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Message() {
+export default function Message({ message: { user, text }, name }) {
+  const [isSentByUser, setIsSentByUser] = useState(false);
+  const trimmedName = name.trim().toLocaleLowerCase();
+
+  if (user === trimmedName) {
+    setIsSentByUser(true);
+  }
+
   return (
-    <div>
-      
-    </div>
+    isSentByUser
+    ? (
+      <div className="message-container justify-end">
+        <p className="sent-text">{trimmedName}</p>
+        <div className="message-box bg-dark">
+          <p className="message-text color-white">{text}</p>
+        </div>
+      </div>
+    )
+    : (
+      <div className="message-container justify-end">
+        <div className="message-box bg-dark">
+          <p className="message-text color-black">{text}</p>
+        </div>
+        <p className="sent-text">{user}</p>
+      </div>
+    )
   )
 }
