4165e122a5131bbfaa89c11d0c7d1ae3eed56c0a
Simon <simonsch.tz@gmail.com>
2020-06-09 03:48:55 +0200

    Work on creating account forms and stuff

--- client/src/MyProfile.js
+++ client/src/MyProfile.js
@@ -1,20 +1,132 @@
 import React, { useState, useEffect } from 'react'
 
-export default function MyProfile(id) {
+export default function MyProfile(props, id) {
+    const user = props.props.cookies.user
     // const param = props.match.params.id
-    const [userData, setUserData] = useState([]);
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [pw, setPW] = useState("");
+    const [role, setRole] = useState("");
+
     useEffect(() => {
-        fetch(`http://localhost:3000/users/${id.id}`, {
+        fetch(`http://localhost:3000/users/${props.id}`, {
             credentials: "include",
             // headers: {"Content-Type": "application/json"}
         })
             .then(res => res.json())
-            .then(data => setUserData(data.user)) 
+            .then(data => {
+                setFirstName(data.user.firstName)
+                setLastName(data.user.lastName)
+                setUserName(data.user.userName)
+                setEmail(data.user.email)
+                setPW(data.user.pw)
+                setRole(data.user.role)
+                console.log(data)
+            }) 
     }, [])
+    
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const body = {
+            "firstName": firstName,
+            "lastName": lastName,
+            "userName": userName,
+            "email": email,
+            "pw": pw,
+            "role": role
+        };
+        //Put request
+        const putData = async (url, data) => {
+            const response = await fetch(url, {
+                method: "PUT",
+                credentials:"include",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data)
+            })
+            return response.json()
+        }
+        putData(`http://localhost:3000/archive/${props.id}`, body)
+            .then(data => { if (!data.success) 
+                { console.log(data) } else {
+                    window.location.assign(`/user`)
+                } })
+    }
+
+
+
+    const handleFormInput = event => {
+        const id = event.target.id;
+        const input = event.target.value;
+        switch (id) {
+            case "firstName":
+                setFirstName(input)
+                break;
+            case "lastName":
+                setLastName(input)
+                break;
+            case "userName":
+                setUserName(input)
+                break;
+            case "email":
+                setEmail(input)
+                break;
+            case "pw":
+                setPW(input)
+                break;
+            case "role":
+                setRole(input)
+                break;
+            default: console.log("Sign up Input in SignUp.js ran through without effect")
+        }
+    };
+
     return (
         <div>
-            <h2>My Profile</h2>
-            <button onClick={()=> console.log(userData)}>Console.log</button>
+                <div className="input-form not-stream-component">
+                <h2>my profile</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="grid-container">
+                        <label htmlFor="firstName">
+                            <span className="required">*</span>first name
+                        <input type="text" id="firstName" placeholder="First Name" value={firstName} onChange={handleFormInput} />
+                        </label>
+                        <label htmlFor="lastName">
+                            <span className="required">*</span>last name
+                        <input type="text" id="lastName" placeholder="Last Name" value={lastName} onChange={handleFormInput} />
+                        </label>
+                        <label htmlFor="userName">
+                            <span className="required">*</span>user name
+                        <input type="text" id="userName" placeholder="User Name" value={userName} onChange={handleFormInput} />
+                        </label>
+                        <label htmlFor="email">
+                            <span className="required">*</span>email
+                        <input type="text" id="email" placeholder="Email" value={email} onChange={handleFormInput} />
+                        </label>
+                        <label htmlFor="pw">
+                            <span className="required">*</span>password
+                        <input type="text" id="pw" placeholder="At least 8 signs long" value={pw} onChange={handleFormInput} />
+                        </label>
+                        {user && user.role === 'Admin' ?
+                        <label htmlFor="role">
+                            <span className="required">*</span>role
+                        <select id="role" value={role} onChange={handleFormInput}>
+                            <option>Admin</option>
+                            <option>Host</option>
+                        </select>
+                        </label>
+                        : null }
+                    </div>
+                    <div className="submit-button">
+                        <input type="submit" value="Save" /><span className="required">* Required</span>
+                    </div>
+                </form>
+            </div>
+            
         </div>
     )
 }
--- client/src/StaffOnly.js
+++ client/src/StaffOnly.js
@@ -1,32 +1,41 @@
 import React, {useState} from 'react'
 import MyProfile from './MyProfile';
 
-import { Link } from 'react-router-dom';
+import CreateUser from './CreateUser';
 export default function UserProfile(props) {
     
     const [showProfile, setShowProfile] =useState(false);
+    const [showCreate, setShowCreate] =useState(false);
     const user = props.cookies.user
     const id= user._id
-    
+    console.log(user.role)
     const handleLogOut = () => {
         props.removeCookie('user', {path: "/"});
         props.removeCookie('x-auth', {path: "/"});
         window.location.assign(`/login`)
     };
     return (
-        <div className="not-stream-component">
+        <div className="not-stream-component staff-only">
             <h2>Wassuuuuuuuup {user.firstName} </h2>
             {showProfile ? 
                 <button type="button" onClick={() => setShowProfile(false)}>cancel</button>:
                 <button type="button" onClick={() => setShowProfile(true)}>edit profile</button> 
             }
             {showProfile ? 
-                <MyProfile id={id} />:
+                <MyProfile props={props} id={id} />:
                 null
             }
             
+            {showCreate ? 
+                <button type="button" onClick={() => setShowCreate(false)}>cancel</button>:
+                <button type="button" onClick={() => setShowCreate(true)}>create new profile</button> 
+            }
+            {showCreate ? 
+                <CreateUser />:
+                null
+            }
             {/* {user ? */}
-                <button onClick={handleLogOut}>Log Out</button>
+                <button onClick={handleLogOut}>log out</button>
             {/* : null } */}
             
         </div>
