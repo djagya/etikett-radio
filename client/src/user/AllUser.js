bb54b372b293f0c7f0608417e542ffb235e6879d
Simon <simonsch.tz@gmail.com>
2020-06-10 19:29:50 +0200

    userData shows up on page

--- client/src/user/AllUser.js
+++ client/src/user/AllUser.js
@@ -4,6 +4,9 @@ import {Context} from "../Context";
 export default function AllUser() {
     const [userData, setUserData] = useState([]);
 
+    let sortedData = userData.sort((userA, userB)=>(userA.firstName < userB.firstName)? -1 : 1)
+
+
 
     useEffect(() => {
 
@@ -14,25 +17,47 @@ export default function AllUser() {
             credentials: "include"
         })
             .then(res => res.json())
-            .then(data => setUserData(data.users.sort((userB, userA)=>userA.firstName - userB.firstName))) 
+            .then(data => setUserData(data.users)) 
     }, [])
 
-    const renderLi =  user => {
-        console.log(user)
-    }
+   
 
-    const renderUsers = userData => {
-        userData.map(user =>
-            renderLi(user)
-            )
+    const renderLi = (userData) => {
+        
+        // Render login and create account links
+        if (userData.status === 403) return (<h2>please log in as admin</h2>)
 
-    }
+        //Because first time the code is running, userData will be an empty array
+        if (userData.length === 0) return null; 
+        console.log(userData)
+        return userData.map((el, i) => (
+            <li key={i}>
+                <ul>
+                    <li>First Name: {el.firstName}</li>
+                    <li>Last Name: {el.lastName}</li>
+                    <li>User Name: {el.userName}</li>
+                    <li>Email: {el.email}</li>
+                    <li>Role: {el.role}</li>
+                </ul>
+            </li>
+        ));
+    };
 
-    console.log(userData)
     return (
         <div className="not-stream-component">
             <h2>All Users</h2>
-            {renderUsers(userData)}
+            <div className="user-list">
+                <ul>
+                    <li>first name</li>
+                    <li>sast name</li>
+                    <li>user name</li>
+                    <li>email</li>
+                    <li>role</li>
+                </ul>
+                <ul>
+                {renderLi(sortedData)}
+                </ul>
+            </div>
         </div>
     )
 }
--- client/src/user/StaffOnly.js
+++ client/src/user/StaffOnly.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from 'react'
+import React, { useContext} from 'react'
 import {Context} from "../Context"
 import { Redirect } from 'react-router-dom';
 
