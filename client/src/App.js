6e4ee4dee7fde6ed1186b624cb8289deedf1bcbe
fran <francisco.chiarino@gmail.com>
2020-06-10 15:22:58 +0200

    Still need to work on some performance issues

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ function App() {
       <div className="App">
 
         <div className="noise" >
-          <Noisy />
+          {/* <Noisy /> */}
         </div>
 
         <div className="stream-page">
