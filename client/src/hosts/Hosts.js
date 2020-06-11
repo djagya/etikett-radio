b753de6bfeac06223431358b43319da73a0bdf62
Simon <simonsch.tz@gmail.com>
2020-06-11 16:34:56 +0200

    Recover files after merge

---
+++ client/src/hosts/Hosts.js
@@ -0,0 +1,20 @@
+import React from "react";
+import EmblaCarouselComponent from "./EmblaCarouselComponent";
+import '../App.scss'
+
+
+const Hosts = () => (
+  
+    <div className="not-stream-component hosts">
+    <h2>hosts</h2>
+      <EmblaCarouselComponent>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+      </EmblaCarouselComponent>
+    </div>
+
+);
+export default Hosts;
