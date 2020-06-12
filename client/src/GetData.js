232f5391f9927678899476212ef10a50eee03e0d
Simon <simonsch.tz@gmail.com>
2020-06-12 03:20:08 +0200

    GetData is now a thing

---
+++ client/src/GetData.js
@@ -0,0 +1,8 @@
+export default async function GetData(url) {
+
+    const response = await fetch(url, {
+        credentials:"include",
+        headers: {"Content-Type": "application/json"}
+    })
+    return response.json()
+}
