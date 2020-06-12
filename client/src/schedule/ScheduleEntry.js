99b3c0436789b7e8698e7d1b4089049f24772ace
Adrienn <adrienntollas@gmail.com>
2020-06-12 12:55:23 +0200

    add aria to schedule component

--- client/src/schedule/ScheduleEntry.js
+++ client/src/schedule/ScheduleEntry.js
@@ -1,12 +1,12 @@
-import React, { useState, useContext}  from 'react';
+import React, { useState, useContext } from 'react';
 import moment from "moment";
-import {Context} from "../Context";
+import { Context } from "../Context";
 import ScheduleEditForm from './ScheduleEditForm';
 import { useCookies } from 'react-cookie';
 
 
 export default function ScheduleEntry(props) {
-    const [showEdit, setShowEdit] =useState(false);
+    const [showEdit, setShowEdit] = useState(false);
     const context = useContext(Context)
     const data = props.data
     const [cookies, setCookie, removeCookie] = useCookies(['user'])
@@ -25,41 +25,41 @@ export default function ScheduleEntry(props) {
         }
     };
 
-    
-     ///////For interactivity//////////
-     let isLive = "";
-     const showStart = moment(data.from);
-     const showEnd = moment(data.to);
-     const currTime = moment();
-     
-     if (currTime > showEnd) {
-         isLive = "was-live";
-     }
-     if (showStart < currTime && currTime < showEnd) {
-         isLive = "is-live";
-     }
-     ///////For interactivity//////////
+
+    ///////For interactivity//////////
+    let isLive = "";
+    const showStart = moment(data.from);
+    const showEnd = moment(data.to);
+    const currTime = moment();
+
+    if (currTime > showEnd) {
+        isLive = "was-live";
+    }
+    if (showStart < currTime && currTime < showEnd) {
+        isLive = "is-live";
+    }
+    ///////For interactivity//////////
     return (
         <div >
-         {cookies.user && cookies.user.role === 'Admin' ?
-            <div className="button-container controls">
-                {showEdit ? 
-                <button type="button" onClick={() => setShowEdit(false)}>cancel</button>:
-                <button type="button" onClick={() => setShowEdit(true)}>edit</button> 
-                }
-                <input className="check-delete" name={data._id} type="checkbox" onChange={handleIDs}></input>
-            </div>
-        : null }
-            {showEdit? 
+            {cookies.user && cookies.user.role === 'Admin' ?
+                <div className="button-container controls">
+                    {showEdit ?
+                        <button type="button" onClick={() => setShowEdit(false)}>cancel</button> :
+                        <button type="button" onClick={() => setShowEdit(true)}>edit</button>
+                    }
+                    <input className="check-delete" name={data._id} type="checkbox" onChange={handleIDs} role="checkbox"></input>
+                </div>
+                : null}
+            {showEdit ?
                 <ScheduleEditForm data={data} /> :
-                <ul className="day-details">  
+                <ul className="day-details">
                     <li className={`${isLive}`}>{data.show}</li>
                     <li className={`${isLive}`}>
                         {showStart.format("H:mm")} - {showEnd.format("H:mm")}
                     </li>
                 </ul>
             }
-            
+
         </div>
     )
 }
