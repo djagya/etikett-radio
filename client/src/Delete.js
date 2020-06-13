9400c0b757af14f7f37a6505be0d523f3664d760
fran <francisco.chiarino@gmail.com>
2020-06-13 11:04:51 +0200

    Delete function is now working

--- client/src/Delete.js
+++ client/src/Delete.js
@@ -1,24 +1,44 @@
 
 export default function Delete(checkedIDs, route) {
 
-    checkedIDs.map(id => {
-        const deleteData = async (url) => {
-            const response = await fetch(url, {
-                method: "DELETE",
-                credentials: "include",
-                headers: {"Content-Type": "application/json"},
-            })
-            return response.json()
-        }
-        deleteData(`http://localhost:3000/${route}/${id}`)
-            .then(data => {
-                // data.success ? console.log("Deleted Data") : console.log(data)
-                if (data.success) {
-                    alert.success('Data has been deleted.');
-                } else {
-                    alert.error('Something went wrong... Please try again later');
-                }
-            })
+    // checkedIDs.map(id => {
+    //     const deleteData = async (url) => {
+    //         const response = await fetch(url, {
+    //             method: "DELETE",
+    //             credentials: "include",
+    //             headers: {"Content-Type": "application/json"},
+    //         })
+    //         return response.json()
+    //     }
+    //     deleteData(`http://localhost:3000/${route}/${id}`)
+    //         .then(data => {
+    //             // data.success ? console.log("Deleted Data") : console.log(data)
+    //             if (data.success) {
+    //                 alert.success('Data has been deleted.');
+    //             } else {
+    //                 alert.error('Something went wrong... Please try again later');
+    //             }
+    //         })
+    // })
 
-    })
+    // Options
+    const options = {
+        method: 'DELETE',
+        credentials: 'include',
+        headers: {"Content-Type": "application/json"}
+    }
+
+    const getEachResponse = () => {
+        let responseArray = [];
+
+        checkedIDs.forEach(id => {
+            fetch(`http://localhost:3000/${route}/${id}`, options)
+                .then(res => res.json())
+                .then(data => responseArray.push(data.success))
+                .catch(error => responseArray.push(error))
+        })
+        console.log(responseArray)
+        return responseArray
+    }
+    console.log('getEachResponse: ', getEachResponse())
 }
