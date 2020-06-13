468d9523dfb8c3e8a808c6f85955a614cfc09d44
Simon <simonsch.tz@gmail.com>
2020-06-13 16:44:22 +0200

    Refactor css slides to work in react

--- client/src/hosts/HostCarousel.js
+++ client/src/hosts/HostCarousel.js
@@ -8,6 +8,7 @@ import HostShowcase from "./HostShowcase";
 
 export default function Hosts() {
     const [hostData, setHostData] = useState([])
+    let [num, setNum] = useState(1)
     let sortedData = []
     console.log(hostData)
     useEffect(() => {
@@ -18,17 +19,17 @@ export default function Hosts() {
     if (hostData.length !== 0) sortedData = hostData.sort((hostA, hostB)=>(hostA.hostName < hostB.hostName)? -1 : 1)
 
 
-    const renderLabels = (host, i) => {
-        const num = i+1;
+    // const renderLabels = (host, i) => {
+ 
 
-        return (
-            <li key={i} className="control-container">
-            <label htmlFor={`carousel-${num === 1 ? sortedData.length : num -1}`} className={`carousel-control prev control-${num}`}>‹</label>
-            <label htmlFor={`carousel-${num === sortedData.length ? 1 : num +1}`} className={`carousel-control prev control-${num}`}>›</label>
-            </li>
-        )
-    }
-
+    //     return (
+    //         <li key={i} className="control-container">
+    //         <label htmlFor={`carousel-${num === 1 ? setNum(sortedData.length) : setNum(num -1)}`} className={`carousel-control prev control-${num}`}>‹</label>
+    //         <label htmlFor={`carousel-${num === setNum(sortedData.length) ? setNum(1) : setNum(num +1)}`} className={`carousel-control prev control-${num}`}>›</label>
+    //         </li>
+    //     )
+    // }
+console.log(num)
 
     const renderIndicator = (host, i) => (
         <li key={i}>
@@ -43,9 +44,13 @@ export default function Hosts() {
         <div className="carousel">
             <div className="carousel-inner">
                 <HostShowcase hosts={sortedData} />
-                <ol>
+                {/* <ol>
                 {sortedData.map((host, i) => renderLabels(host, i))}
-                </ol>
+                </ol> */}
+                <li className="control-container">
+                <label htmlFor={`carousel-${num === 1 ? sortedData.length : (num +1)}`} onClick={() => setNum(num++)} className={`carousel-control prev control-${num}`}>‹</label>
+                <label htmlFor={`carousel-${num === sortedData.length ? 1 : (num -1)}`} onClick={() => setNum(num--)} className={`carousel-control prev control-${num}`}>›</label>
+                </li>
                 <ol className="carousel-indicators">
                 {sortedData.map((host, i) => renderIndicator(host, i))}
                 </ol>
