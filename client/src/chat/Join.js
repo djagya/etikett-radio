95d420b7f355f4cf872087ca8153ac1267045d5f
fran <francisco.chiarino@gmail.com>
2020-06-15 21:35:54 +0200

    Commented handleSubmit on Join.js

--- client/src/chat/Join.js
+++ client/src/chat/Join.js
@@ -8,37 +8,49 @@ export default function Join({setName}) {
   const handleSubmit = e => {
     e.preventDefault();
 
+    // Prevent user from having the same name as host
     fetch('http://localhost:3000/host')
       .then(res => res.json())
       .then(data => {
         let match = false;
+
+        // Compare user's chatName with hostName
         data.host.map(({hostName}) => {
           if (hostName.trim().toLocaleLowerCase() === nameInput.trim().toLocaleLowerCase()) {
             match = true;
             return match
           }
         })
+        // Return result
         return match
       })
       .then((match) => {
+
         if (!match) {
+          // Prevent user from having the same name as a chat user
           fetch('http://localhost:3000/chat')
             .then(res => res.json())
             .then(data => {
+
+              // Compare user's chatName with chatUsers
               data.chatUsers.map(({name}) => {
                 if (name === nameInput.trim().toLocaleLowerCase()) {
                   match = true;
                   return match; 
                 }
               });
+              // Return result
               return match; 
             })
             .then((match) => {
+
               if (!match) {
+                // Add user
                 setName(nameInput);
                 sessionStorage.setItem('name', nameInput);      
               } else {
                 alert.error('User is already taken.');
+                return;
               }
             })  
         } else {
