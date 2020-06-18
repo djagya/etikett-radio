8d764380c80faa6d51419dd80cca9a8b7e199ddf
Simon <simonsch.tz@gmail.com>
2020-06-18 08:17:28 +0200

    Update userRoute

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,5 +1,5 @@
 const Route = require("express").Router();
-const { getUserById, getUsers, postUser, putUser, deleteUser, login, sendEmail } = require("../controllers/usersController");
+const { getUserById, getUsers, postUser, putUser, deleteUser, login, sendEmail, patchUser } = require("../controllers/usersController");
 const { validUserInputs } = require("../middleware/usersValidator");
 const auth = require("../middleware/tokenAuthenticator");
 const {isAdmin, isHostAdmin} = require("../middleware/rolesAuthenticator");
@@ -8,10 +8,10 @@ Route.get("/", auth, isAdmin, getUsers);
 Route.post("/createuser", auth, isAdmin, validUserInputs(), postUser);
 Route.post("/login", auth, login);
 Route.post("/contact", sendEmail);
-//Alternative syntax for practice purposes
 Route.route("/:id")
     .get(auth, getUserById)
     .put(auth, isHostAdmin, putUser)
+    .patch(auth, isHostAdmin, patchUser)
     .delete(auth, isAdmin, deleteUser);
 
 module.exports = Route;
