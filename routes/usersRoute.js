c693087725d08962a3cea5abcab54aae8c855d22
Simon <simonsch.tz@gmail.com>
2020-06-18 10:25:29 +0200

    user patch is working

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -4,12 +4,12 @@ const { validUserInputs } = require("../middleware/usersValidator");
 const auth = require("../middleware/tokenAuthenticator");
 const {isAdmin, isHostAdmin} = require("../middleware/rolesAuthenticator");
 
-Route.get("/", auth, isAdmin, getUsers);
+Route.get("/", auth, getUsers);
 Route.post("/createuser", auth, isAdmin, validUserInputs(), postUser);
 Route.post("/login", auth, login);
 Route.post("/contact", sendEmail);
 Route.route("/:id")
-    .get(auth, getUserById)
+    .get(auth, isHostAdmin, getUserById)
     .put(auth, isHostAdmin, putUser)
     .patch(auth, isHostAdmin, patchUser)
     .delete(auth, isAdmin, deleteUser);
