3e2f9d0e07ca071fe0b2e5eb15a8eca0688cbade
Adrienn <adrienntollas@gmail.com>
2020-06-22 18:54:04 +0200

    make logo clickable and swith to dark image

--- client/src/App.js
+++ client/src/App.js
@@ -58,9 +58,9 @@ function App(props) {
   const [infoBarMessage, setInfoBarMessage] = useState("");
   const [infoID, setInfoID] = useState("");
   const [name, setName] = useState('');
-  const [gapClass, setGapClass] =useState("big-gap");
-  const [pathName, setPathName] =useState("/")
-  const [gradient, setGradient] =useState("gradient");
+  const [gapClass, setGapClass] = useState("big-gap");
+  const [pathName, setPathName] = useState("/")
+  const [gradient, setGradient] = useState("gradient");
 
   // Media Queries
   const isMobileWidth = useMediaQuery({ maxWidth: 600 });
@@ -108,6 +108,7 @@ function App(props) {
             <div className="solar-system">
               <SolarSystem />
             </div>
+            <div className="etikett-black"></div>
 
             <div className="stream-page">
               <Header name={name} setName={setName} isMobileWidth={isMobileWidth} isMobileDevice={isMobileDevice} />
@@ -117,11 +118,11 @@ function App(props) {
 
               {/*Placeholder for / route so we don't land on Error component*/}
               <Route exact path="/">
-                { isMobileDevice ? <Redirect to="/schedule" /> : <Home/> }
+                {isMobileDevice ? <Redirect to="/schedule" /> : <Home />}
               </Route>
-              
+
               {/* User Related */}
-              <Route exact path="/login" render={(props) => <LogIn {...props} setCookie={setCookie} cookies={cookies}  setName={setName} removeCookie={removeCookie} />} />
+              <Route exact path="/login" render={(props) => <LogIn {...props} setCookie={setCookie} cookies={cookies} setName={setName} removeCookie={removeCookie} />} />
               <Route exact path="/user/all" render={(props) => <AllUser {...props} cookies={cookies} />} />
               <Route exact path="/user/createuser" render={(props) => <CreateUser {...props} setCookie={setCookie} cookies={cookies} />} />
               <Route exact path="/user/:id" render={(props) => <StaffOnly {...props} removeCookie={removeCookie} cookies={cookies} setName={setName} />} />
