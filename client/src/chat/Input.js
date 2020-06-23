bb91f04bc18168a1d7109879762ebc7f2757f3d6
fran <francisco.chiarino@gmail.com>
2020-06-23 20:26:14 +0200

    Row are added and removed properly

--- client/src/chat/Input.js
+++ client/src/chat/Input.js
@@ -4,15 +4,19 @@ export default function Input({ text, setText, sendMessage }) {
   const [rows, setRows] = useState(1);
 
   useEffect(() => {
-    if (text.length >= 23) {
+    const charLength = 23;
+    if (text.length >= charLength * 2) {
+      setRows(3)
+    } else if (text.length >= charLength) {
       setRows(2)
+    } else {
+      setRows(1)
     }
   }, [text])
 
   return (
     <div className="Input">
-      <form onSubmit={sendMessage} className="chat-form">
-
+      <form onSubmit={sendMessage} className="chat-form button-container">
         <textarea rows={rows} id="input-message-textarea" maxLength="500" type="text" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
         <button className="chat-button" type="submit">send</button>
       </form>
