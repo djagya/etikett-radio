5ae55c0492c203e34912430ec489f2093707348a
fran <francisco.chiarino@gmail.com>
2020-06-23 19:28:33 +0200

    Textarea is responsive

--- client/src/chat/Input.js
+++ client/src/chat/Input.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Input({ text, setText, sendMessage }) {
+  const [rows, setRows] = useState(1);
+
+  useEffect(() => {
+    if (text.length >= 23 && rows < 2) {
+      setRows(2)
+    }
+  }, [text])
+
   return (
     <div className="Input">
       <form onSubmit={sendMessage} className="chat-form button-container">
 
-        <input type="text" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
+        <textarea rows={rows} id="input-message-textarea" maxLength="500" type="text" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
         <button className="chat-button" type="submit">send</button>
       </form>
     </div>
