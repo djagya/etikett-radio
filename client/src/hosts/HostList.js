44318ac9b147c31b3c4dd4f3a80b1acbe4c68ef3
Simon <simonsch.tz@gmail.com>
2020-06-23 17:44:29 +0200

    Fix Post/Put link in EditHostForm

--- client/src/hosts/HostList.js
+++ client/src/hosts/HostList.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Context } from "../Context";
+import GetData from "../GetData";
+import DocumentTitle from 'react-document-title';
 
 export default function HostList() {
     const context = useContext(Context);
+    const [hostData, setHostData] = useState([])
+
+    useEffect(() => {
+        GetData("/host")
+            .then(data => setHostData(data.host.filter(host => host.isActive)))
+    }, [])
+    console.log(hostData)
     return (
         <div className={` all-list ${context.gapClass}`}>
             <h2>Host List</h2>
