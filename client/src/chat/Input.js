66e8c5a5a0b009ea896eda0e46c6179e3313c1cd
fran <francisco.chiarino@gmail.com>
2020-06-23 21:52:45 +0200

    Messages are sent with enter

--- client/src/chat/Input.js
+++ client/src/chat/Input.js
@@ -5,8 +5,8 @@ export default function Input({ rows, text, setText, sendMessage }) {
   return (
     <div className="Input">
       <form onSubmit={sendMessage} className="chat-form button-container">
-        <textarea rows={rows} id="input-message-textarea" maxLength="500" type="text" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
-        <button className="chat-button" type="submit">send</button>
+        <textarea rows={rows} id="input-message-textarea" onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null} maxLength="500" type="text" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
+        <button className="chat-button" type="submit" >send</button>
       </form>
     </div>
   )
