20c1614ff00ec9415aaa78f3e57d1ef4b2adf800
fran <francisco.chiarino@gmail.com>
2020-06-24 15:52:01 +0200

    Wrote ErrorBoundary

---
+++ client/src/ErrorBoundary.js
@@ -0,0 +1,24 @@
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {    
+  // Update state so the next render will show the fallback UI.    
+  return { hasError: true, error };  
+  }
+
+  componentDidCatch(error, errorInfo) {    
+  // You can also log the error to an error reporting service    
+    console.log(error, errorInfo);  
+  }
+
+  render() {
+    if (this.state.hasError) {      
+    // You can render any custom fallback UI      
+    return <h3>Something went wrong: {this.state.error.message}</h3>;    
+    }
+    return this.props.children; 
+  }
+}
