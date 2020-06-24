beaf209be10275a24036ca9d36fba9485493b1f2
Simon <simonsch.tz@gmail.com>
2020-06-24 17:20:52 +0200

    Aligned send button with chat input

--- client/src/chat/Input.js
+++ client/src/chat/Input.js
@@ -13,7 +13,7 @@ export default function Input({ text, setText, sendMessage }) {
     <div className="Input">
       <form onSubmit={sendMessage} className="chat-form button-container">
         <textarea ref={input} id="input-message-textarea" onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null} maxLength="500" type="text" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
-        <button className="chat-button" type="submit" >send</button>
+        <button className="chat-button chat-send" type="submit" >send</button>
       </form>
     </div>
   )
