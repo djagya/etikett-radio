36342e101162dc7280dcab89f8e18f9eadd56776
Adrienn <adrienntollas@gmail.com>
2020-06-25 17:42:56 +0200

    style alert2

--- client/src/index.js
+++ client/src/index.js
@@ -8,40 +8,40 @@ import { transitions, positions, Provider as AlertProvider } from 'react-alert'
 const options = {
   position: positions.MIDDLE,
   timeout: 0,
-  offset: '-50px',
+  offset: '0',
   transition: transitions.SCALE,
 }
 const alertStyle = {
   backgroundColor: '#010508',
   color: '#fff',
-  padding: '10px',
+  fontSize: '16px',
+  padding: '18px',
   textTransform: 'lowercase',
   display: 'flex',
-  justifyContent: 'space-between',
+  justifyContent: 'center',
   alignItems: 'center',
   width: '300px',
   height: "70px",
   border: "1px solid #467883",
   boxSizing: 'border-box',
-  // boxShadow: '0 14px 28px rgba(255, 255, 255, 0.25)'
+  borderRadius: "2px"
 
 }
 const buttonStyle = {
-  marginLeft: '20px',
+  marginLeft: '25px',
   border: 'none',
-  height: "10px",
   display: 'flex',
   alignItems: 'center',
   cursor: 'pointer',
-  color: '#fff'
+  color: '#010508'
 }
 const AlertTemplate = ({ style, options, message, close }) => (
   <div style={{ ...alertStyle, ...style }}>
-    {options.type === 'info' && '!'}
-    {options.type === 'success' && ':)'}
+    {options.type === 'info' && ''}
+    {options.type === 'success' && ''}
     {options.type === 'error' && ''}
     {message}
-    <button onClick={close} style={buttonStyle}>X</button>
+    <button onClick={close} style={buttonStyle}><i className="far fa-times-circle"></i></button>
   </div>
 )
 
