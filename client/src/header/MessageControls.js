b81794c5e329a0559a7b45867925f3894cde849f
fran <francisco.chiarino@gmail.com>
2020-06-25 16:27:44 +0200

    Wrote function to fetch songTitle

--- client/src/header/MessageControls.js
+++ client/src/header/MessageControls.js
@@ -1,15 +1,40 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Context } from "../Context";
 import moment from "moment";
+import GetData from '../GetData';
 
 
 export default function MessageControls({ source, radio, icon, volume, handlePlayBtn, handleAudio, handleVolume }) {
   const context = useContext(Context);
   const [time, setTime] = useState(moment().format("h:mm:ss a"))
   const timer = () => setTime(moment().format("H:mm:ss"))
+  const [songName, setSongName] = useState(null);
 
   useEffect(() => {
     const id = setInterval(timer, 1000);
+    GetData('https://s9.myradiostream.com/44782/stats?json=1')
+      .then(data => {
+        let titleWords;
+        let sanitizedTitle = '';
+
+        // Separate words
+        if (data.songtitle.includes('_')) {
+          titleWords = data.songTitle.split('_');
+        }
+        if (data.songtitle.includes(' ')) {
+          titleWords = data.songTitle.split(' ');
+        }
+
+        // Sanitize word
+        titleWords.forEach(word => {
+          let sanitizedWord = word[0].toLocaleUpperCase() + word.substring(1).toLocaleLowerCase();
+          sanitizedTitle += sanitizedWord;
+        })
+
+        // Set title
+        setSongName(sanitizedTitle);
+      })
+
     return () => clearInterval(id);
   }, [time]);
 
@@ -23,7 +48,7 @@ export default function MessageControls({ source, radio, icon, volume, handlePla
         </div>
         : null}
       <div className="message">
-        <span className="moving-text"> {time} -- {context.infoBarMessage}</span>
+        <span className="moving-text"> {time} -- {songName} -- {context.infoBarMessage}</span>
       </div>
 
     </section>
