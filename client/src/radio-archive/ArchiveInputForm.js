f47a397eefad1846f1ff48f92112991e2021e18d
Simon <simonsch.tz@gmail.com>
2020-06-25 12:26:15 +0200

    Starting with host implementation on archive page, some styling refactoring

--- client/src/radio-archive/ArchiveInputForm.js
+++ client/src/radio-archive/ArchiveInputForm.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { useAlert } from 'react-alert';
+import PostData from "../PostData";
+import GetData from "../GetData";
 
 export default function ArchiveInputForm() {
+    const [hostData, setHostData] = useState([]);
     const [host, setHost] = useState("");
+    const [filter, setFilter] = useState("");
     const [show, setShow] = useState("");
     const [genre, setGenre] = useState("");
     const [date, setDate] = useState(new Date().toISOString().substring(0, 10));
@@ -10,6 +14,11 @@ export default function ArchiveInputForm() {
     const [img, setImg] = useState("");
     const [description, setDescription] = useState("");
     const alert = useAlert();
+
+    useEffect(() => {
+        GetData("/host")
+            .then(data => setHostData(data.host.sort((hostA, hostB) => (hostA.hostName < hostB.hostName) ? -1 : 1)))
+    }, [])
 
     const handleSubmit = event => {
         event.preventDefault()
@@ -25,18 +34,18 @@ export default function ArchiveInputForm() {
             "description": description,
         };
 
-        const postData = async (url, data) => {
-            const response = await fetch(url, {
-                method: "POST",
-                credentials: "include",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            })
-            return response.json()
-        }
-        postData("/archive/post", body)
+        // const postData = async (url, data) => {
+        //     const response = await fetch(url, {
+        //         method: "POST",
+        //         credentials: "include",
+        //         headers: {
+        //             "Content-Type": "application/json"
+        //         },
+        //         body: JSON.stringify(data)
+        //     })
+        //     return response.json()
+        // }
+        PostData("/archive/post", body)
             .then(data => { reload(data) })
 
         const reload = (data) => {
@@ -52,7 +61,16 @@ export default function ArchiveInputForm() {
         }
     }
 
+    const renderHostOptions = () => {
+        return hostData.map((host,i) => 
 
+           
+            <Fragment key={i}>
+                <option>{host.hostName}</option>
+            </Fragment>
+            
+        )
+    }
 
 
 
@@ -63,6 +81,9 @@ export default function ArchiveInputForm() {
             case "host":
                 setHost(input)
                 break;
+                case "filter":
+                    setFilter(input)
+                    break;
             case "show":
                 setShow(input)
                 break;
@@ -84,42 +105,56 @@ export default function ArchiveInputForm() {
             default: console.log("Archive Input HandleFormInput ran through without effect")
         }
     };
-    const repetitiveInputFields = () => {
-        const fields = ["show", "host", "genre"];
-        const value = [show, host, genre];
-        return fields.map((field, i) => (
-            <label key={i} htmlFor={field}>
-                <span className="required">*</span>{field}
-                <input type="text" id={field} placeholder={field} value={value[i]} onChange={handleFormInput} />
-            </label>
-        ));
-    };
+   
     return (
         <div className="input-form">
             <h2>archive a show.</h2>
             <form className="post-archive" onSubmit={handleSubmit}>
-                <div className="grid-container">
+                <div className ="grid-container">
+                    <div className="archive-input-left">
+                    <label htmlFor="filter">
+                        <span className="required">*</span>host
+                        <input type="text" id="filter" placeholder="filter" value={filter} onChange={handleFormInput} />
+                    </label>
+                    <label className="archive-select-container" htmlFor="host">
+                        <select id="host" value={host} size="17" onChange={handleFormInput}>
+                            {renderHostOptions()}
+                        </select>
+                    </label>
+                    
+                    
+                    </div>
 
-                    {repetitiveInputFields()}
-                    <label htmlFor="date">
-                        <span className="required">*</span>date
-                    <input type="date" id="date" placeholder="yyyy-mm-dd" value={date} onChange={handleFormInput} />
-                    </label>
-                    <label htmlFor="link">
-                        <span className="required">*</span>soundcloud/mixcloud
-                    <input type="url" id="link" placeholder="link" value={link} onChange={handleFormInput} />
-                    </label>
-                    <label htmlFor="img">
-                        <span className="required">*</span>artwork
-                    <input type="url" id="img" placeholder="link" value={img} onChange={handleFormInput} />
-                    </label>
-                    <label className="describe" htmlFor="description">
-                        description
-                    <textarea type="text" id="description" placeholder="Describe the show (e.g. discussed topics, featured artists, track list etc.)" onChange={handleFormInput} defaultValue={description} />
-                    </label>
-                </div>
-                <div className="submit-button">
-                    <input type="submit" value="save" role="button" /><span className="required">* required</span>
+
+                    <div className="archive-input-right">
+                        <label htmlFor="show">
+                            <span className="required">*</span>show
+                            <input type="text" id="show" placeholder="show" value={show} onChange={handleFormInput} />
+                        </label>
+                        <label htmlFor="genre">
+                        <span className="required">*</span>genre
+                            <input type="text" id="genre" placeholder="genre" value={genre} onChange={handleFormInput} />
+                        </label>
+                        <label htmlFor="date">
+                            <span className="required">*</span>date
+                        <input type="date" id="date" placeholder="yyyy-mm-dd" value={date} onChange={handleFormInput} />
+                        </label>
+                        <label htmlFor="link">
+                            <span className="required">*</span>soundcloud/mixcloud
+                        <input type="url" id="link" placeholder="link" value={link} onChange={handleFormInput} />
+                        </label>
+                        <label htmlFor="img">
+                            <span className="required">*</span>artwork
+                        <input type="url" id="img" placeholder="link" value={img} onChange={handleFormInput} />
+                        </label>
+                        <label className="describe" htmlFor="description">
+                            description
+                        <textarea type="text" id="description" placeholder="Describe the show (e.g. discussed topics, featured artists, track list etc.)" onChange={handleFormInput} defaultValue={description} />
+                        </label>
+                    </div>
+                    <div className="submit-button">
+                        <input type="submit" value="save" role="button" /><span className="required">* required</span>
+                    </div>
                 </div>
             </form>
         </div>
