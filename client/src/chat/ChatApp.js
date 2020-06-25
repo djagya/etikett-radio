3ceebc0cea7efb7d560fd263b60b6272b4611bad
fran <francisco.chiarino@gmail.com>
2020-06-25 11:50:42 +0200

    Running tests on header

--- client/src/chat/ChatApp.js
+++ client/src/chat/ChatApp.js
@@ -9,9 +9,7 @@ function ChatApp({ name, setName }) {
   const [chatWindow, setChatWindow] = useState('chat-app-chat');
   const [cookies, setCookie, removeCookie] = useCookies(['user', 'name']);
   const room = 'etikett chat';
-  useEffect(() => {
-    throw new Error('Chat could not load. Try reloading the page, or contact an admin.')
-  }, [])
+
   useEffect(() => {
     const name = cookies.name;
     if (name) {
