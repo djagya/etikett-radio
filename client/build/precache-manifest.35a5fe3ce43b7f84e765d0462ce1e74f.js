388299e4454ee89475bfb5a9c89a8a15b35e5aab
Simon <simonsch.tz@gmail.com>
2020-06-29 22:30:15 +0200

    Build Version #21

---
+++ client/build/precache-manifest.35a5fe3ce43b7f84e765d0462ce1e74f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319d712716443311a0d53582f89c27fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45423a6848445f0254f2",
+    "url": "/static/css/2.85d21689.chunk.css"
+  },
+  {
+    "revision": "a6aae7b7d7ec892a06f3",
+    "url": "/static/css/main.20a19ccc.chunk.css"
+  },
+  {
+    "revision": "45423a6848445f0254f2",
+    "url": "/static/js/2.fbcb195e.chunk.js"
+  },
+  {
+    "revision": "fc01bfec287873dc9e3cbcdcad06419c",
+    "url": "/static/js/2.fbcb195e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6aae7b7d7ec892a06f3",
+    "url": "/static/js/main.70ffec50.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "8e8bc8d27e4386bf5cde3ceb9f17722b",
+    "url": "/static/media/AGaramondPro-Regular.8e8bc8d2.otf"
+  },
+  {
+    "revision": "9986e668fd3020f042bbe78c46e132da",
+    "url": "/static/media/audio.9986e668.png"
+  },
+  {
+    "revision": "cf41dcbd6001abb5a1c3438b96d0dfcb",
+    "url": "/static/media/etikett-black.cf41dcbd.jpg"
+  },
+  {
+    "revision": "045af4f7bb4e0dc3952d5af6085c4c6a",
+    "url": "/static/media/footer-img-1920x600.045af4f7.png"
+  },
+  {
+    "revision": "bfd24c8f4daca9799f1b0b193c7ba9d8",
+    "url": "/static/media/imageonline-co-noise.bfd24c8f.png"
+  }
+]);
