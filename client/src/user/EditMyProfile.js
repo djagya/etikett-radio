ec876d5604e695e56f6ac1bf47474be8400a3a8f
Simon <simonsch.tz@gmail.com>
2020-06-29 14:28:32 +0200

    Hide edit user options behind buttons

--- client/src/user/EditMyProfile.js
+++ client/src/user/EditMyProfile.js
@@ -6,8 +6,8 @@ import PatchData from "../PatchData";
 
 
 export default function MyProfile(props) {
-    const user = props.cookies.user
-    const context = useContext(Context)
+    const user = props.cookies.user;
+    const context = useContext(Context);
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [userName, setUserName] = useState("");
@@ -15,6 +15,8 @@ export default function MyProfile(props) {
     const [pw, setPW] = useState("");
     const [role, setRole] = useState("");
     const alert = useAlert();
+    const [editProfile, setEditProfile] = useState(false);
+    const [editPW, setEditPW] = useState(false);
 
     useEffect(() => {
         fetch(`/users/${context.id}`, {
@@ -108,53 +110,77 @@ export default function MyProfile(props) {
         <div>
             <div className={`${context.gapClass} input-form`}>
                 <h2 id="main">my profile.</h2>
-
-                <form onSubmit={handlePersonalSubmit} role="form">
+                <div>
+                {!editPW && !editProfile ?
                     <div className="button-container">
-                        <button type="button" onClick={() => context.setProfileEdit(false)}>cancel</button>
-                    </div>
-                    <div className="grid-container">
-                        <label htmlFor="firstName">
-                            <span className="required">*</span>first name
-                        <input type="text" id="firstName" placeholder="First Name" value={firstName} onChange={handleFormInput} />
-                        </label>
-                        <label htmlFor="lastName">
-                            <span className="required">*</span>last name
-                        <input type="text" id="lastName" placeholder="Last Name" value={lastName} onChange={handleFormInput} />
-                        </label>
-                        <label htmlFor="userName">
-                            <span className="required">*</span>user name
-                        <input type="text" id="userName" placeholder="User Name" value={userName} onChange={handleFormInput} />
-                        </label>
-                        <label htmlFor="email">
-                            <span className="required">*</span>email
-                        <input type="text" id="email" placeholder="Email" value={email} onChange={handleFormInput} />
-                        </label>
-                        {user && user.role === 'Admin' ?
-                            <label htmlFor="role">
-                                <span className="required">*</span>role
-                        <select id="role" value={role} onChange={handleFormInput}>
-                                    <option>Admin</option>
-                                    <option>Host</option>
-                                </select>
+                        <button type="button" onClick={() => context.setProfileEdit(false)}>back</button>
+                    </div> : null
+                    }
+                    {editProfile ? 
+                        
+                        <form onSubmit={handlePersonalSubmit} role="form">
+                            <div className="button-container staff-only">
+                                <button type="button" onClick={() => setEditProfile(false)}>cancel</button>
+                            </div>
+                            <div className="grid-container">
+                                <label htmlFor="firstName">
+                                    <span className="required">*</span>first name
+                                <input type="text" id="firstName" placeholder="First Name" value={firstName} onChange={handleFormInput} />
+                                </label>
+                                <label htmlFor="lastName">
+                                    <span className="required">*</span>last name
+                                <input type="text" id="lastName" placeholder="Last Name" value={lastName} onChange={handleFormInput} />
+                                </label>
+                                <label htmlFor="userName">
+                                    <span className="required">*</span>user name
+                                <input type="text" id="userName" placeholder="User Name" value={userName} onChange={handleFormInput} />
+                                </label>
+                                <label htmlFor="email">
+                                    <span className="required">*</span>email
+                                <input type="text" id="email" placeholder="Email" value={email} onChange={handleFormInput} />
+                                </label>
+                                {user && user.role === 'Admin' ?
+                                    <label htmlFor="role">
+                                        <span className="required">*</span>role
+                                <select id="role" value={role} onChange={handleFormInput}>
+                                            <option>Admin</option>
+                                            <option>Host</option>
+                                        </select>
+                                    </label>
+                                    : null}
+                            </div>
+                            <div className="submit-button">
+                                <input type="submit" value="save" role="button" /><span className="required">* Required</span>
+                            </div>
+                        </form>  :
+                        editPW? null :
+                        <div className="button-container">
+                            <button type="button" onClick={() => setEditProfile(true)}>edit user data</button>
+                        </div>
+                            
+                    }
+                    
+                    {editPW ?
+                        <form onSubmit={handlePWSubmit} role="form">
+                        <div className="button-container">
+                            <button type="button" onClick={() => setEditPW(false)}>cancel</button>
+                        </div>
+                        <div className="grid-container">
+                            <label htmlFor="pw">
+                                <span className="required">*</span>password
+                                <input type="password" id="pw" placeholder="At least 8 signs long" value={pw} onChange={handleFormInput} />
                             </label>
-                            : null}
-                    </div>
-                    <div className="submit-button">
-                        <input type="submit" value="save" role="button" /><span className="required">* Required</span>
-                    </div>
-                </form>
-                <form onSubmit={handlePWSubmit} role="form">
-                    <div className="grid-container">
-                        <label htmlFor="pw">
-                            <span className="required">*</span>password
-                            <input type="password" id="pw" placeholder="At least 8 signs long" value={pw} onChange={handleFormInput} />
-                        </label>
-                    </div>
-                    <div className="submit-button">
-                        <input type="submit" value="save" role="button" /><span className="required">* Required</span>
-                    </div>
-                </form>
+                        </div>
+                        <div className="submit-button">
+                            <input type="submit" value="save" role="button" /><span className="required">* Required</span>
+                        </div>
+                        </form>  :
+                        editProfile? null :
+                        <div className="button-container">
+                            <button type="button" onClick={() => setEditPW(true)}>change password</button>
+                        </div>
+}
+                </div>
             </div>
         </div>
     )
