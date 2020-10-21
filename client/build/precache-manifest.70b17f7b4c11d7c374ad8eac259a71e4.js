8f9077ee8cbb51f9e08c223a0b39415440733814
Simon <simonsch.tz@gmail.com>
2020-10-21 10:49:13 +0200

    Build Version #41

---
+++ client/build/precache-manifest.70b17f7b4c11d7c374ad8eac259a71e4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5021496b55d343d1c3e6afc15406f5d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a623dc3084717dcfefa",
+    "url": "/static/css/2.85d21689.chunk.css"
+  },
+  {
+    "revision": "bb9edd15d09cc14e8676",
+    "url": "/static/css/main.72846429.chunk.css"
+  },
+  {
+    "revision": "1a623dc3084717dcfefa",
+    "url": "/static/js/2.2ad727e1.chunk.js"
+  },
+  {
+    "revision": "4493f16861130cfeee8e679939d81a1e",
+    "url": "/static/js/2.2ad727e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9edd15d09cc14e8676",
+    "url": "/static/js/main.d739d88c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "1893bda858641ff97ff2d080cf34688f",
+    "url": "/static/media/Icon_neg@2x.1893bda8.png"
+  },
+  {
+    "revision": "f37c3f0bb5f22cea9686edb7fa22302c",
+    "url": "/static/media/Lausanne-300.f37c3f0b.otf"
+  },
+  {
+    "revision": "9e91969099613cccac1aaa82ef5cdb4f",
+    "url": "/static/media/Lausanne-500.9e919690.otf"
+  },
+  {
+    "revision": "e5ccc2503fbbc35bd470014b2a532f7e",
+    "url": "/static/media/Lausanne-700.e5ccc250.otf"
+  },
+  {
+    "revision": "9986e668fd3020f042bbe78c46e132da",
+    "url": "/static/media/audio.9986e668.png"
+  },
+  {
+    "revision": "cf41dcbd6001abb5a1c3438b96d0dfcb",
+    "url": "/static/media/etikett-black.cf41dcbd.jpg"
+  },
+  {
+    "revision": "045af4f7bb4e0dc3952d5af6085c4c6a",
+    "url": "/static/media/footer-img-1920x600.045af4f7.png"
+  },
+  {
+    "revision": "bfd24c8f4daca9799f1b0b193c7ba9d8",
+    "url": "/static/media/imageonline-co-noise.bfd24c8f.png"
+  }
+]);
