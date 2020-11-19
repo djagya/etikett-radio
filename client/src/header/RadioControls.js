e0a820d059f69a7732e5523b198d5919cd290371
Simon <simonsch.tz@gmail.com>
2020-11-19 19:04:18 +0100

    Fix fallback color if noise doesnt get loaded

--- client/src/header/RadioControls.js
+++ client/src/header/RadioControls.js
@@ -9,7 +9,7 @@ export default function RadioControls({ source, radio, icon, volume, handlePlayB
     <section className="controls-container">
       {source === radio ?
         <div className="player-controls">
-          <button className="playPauseBtn" onClick={handlePlayBtn} role="play-pause button"></button>
+          <button className="playPauseBtn" onClick={handlePlayBtn} role="play-pause"></button>
           <img className="audio-icon" src={icon} alt="speaker icon" width="18" onClick={handleAudio} />
           <input className="volumeControl" type="range" min="0" max="1" step="any" value={volume} onChange={handleVolume} role="Volume Control" />
         </div>
