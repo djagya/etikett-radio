44f310bed46361f69f77e9e7dc4282d2365bc8c3
Simon Schötz <simonsch.tz@gmail.com>
2022-02-10 00:05:47 +0100

    Bring back archive track names from myradio streams to see if it works

--- client/src/header/MessageBar.js
+++ client/src/header/MessageBar.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { Context } from "../Context";
-// import moment from "moment";
+import React, { useState, useEffect, useContext } from 'react';
+import { Context } from '../Context';
+import moment from 'moment';
 import GetData from '../GetData';
 import { useAlert } from 'react-alert';
 
@@ -14,87 +14,91 @@ export default function InfoBar() {
    * The clock looked awkward without the sing information so I commented it out, too
    * So the acutal TODO is: Get the meta data shit up and running again!
    */
-  
-  // const [songName, setSongName] = useState(null);
-  // const [time, setTime] = useState(moment().format("h:mm:ss a"))
-  // const timer = () => setTime(moment().format("H:mm:ss"))
 
-  // const renderCurrentSongName = () => {
-  //   return <div>Now playing: {songName}</div>
-  // }
+  const [songName, setSongName] = useState(null);
+  const [time, setTime] = useState(moment().format('h:mm:ss a'));
+  const timer = () => setTime(moment().format('H:mm:ss'));
 
-    useEffect(() => {
-        // let mounted = true;
-    
-        GetData("/infobar")
-          .then(data => {
-            if (!data.success) alert.error("Failed to fetch data, please contact the admin.");
-            if (data.status === 403) {
-              alert.error("Status 403: Forbidden")
-              return
+  const renderCurrentSongName = () => {
+    return <div>Now playing: {songName}</div>;
+  };
+
+  useEffect(() => {
+    let mounted = true;
+
+    GetData('/infobar')
+      .then((data) => {
+        if (!data.success)
+          alert.error('Failed to fetch data, please contact the admin.');
+        if (data.status === 403) {
+          alert.error('Status 403: Forbidden');
+          return;
+        }
+        if (!data.success) {
+          alert.error('Failed to fetch data, please contact the admin');
+          return;
+        }
+        context.setInfoBarMessage(data.infoBar[0].message);
+        // context.setInfoID(data.infoBar[0]._id)
+      })
+      .catch((err) => {
+        alert.error('Failed to fetch info. Please contact the admin.');
+      });
+
+    const getSongName = () => {
+      fetch('https://s9.myradiostream.com/44782/stats?json=1')
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          let titleWords = [];
+          let sanitizedTitle = '';
+
+          // Separate words
+          if (data.songtitle.includes('_')) {
+            titleWords = data.songtitle.split('_');
+          }
+          if (data.songtitle.includes(' ')) {
+            titleWords = data.songtitle.split(' ');
+          }
+
+          // Sanitize word
+          titleWords.forEach((word) => {
+            let sanitizedWord =
+              word[0].toLocaleUpperCase() +
+              word.substring(1).toLocaleLowerCase();
+            sanitizedTitle += sanitizedWord + ' ';
+          });
+
+          // Set title
+          if (mounted) {
+            if (sanitizedTitle === '') {
+              setSongName('Etikett Radio Archive');
+            } else {
+              setSongName(sanitizedTitle);
             }
-            if (!data.success) {
-              alert.error("Failed to fetch data, please contact the admin")
-              return
-            };
-            context.setInfoBarMessage(data.infoBar[0].message)
-            // context.setInfoID(data.infoBar[0]._id)
-          })
-          .catch(err => {
-            alert.error('Failed to fetch info. Please contact the admin.')
-          })
-        
-        // const getSongName = () => {
-        //   fetch('https://cors-anywhere.herokuapp.com/https://s9.myradiostream.com/44782/stats?json=1')
-        //     .then(res => res.json())
-        //     .then(data => {
-        //       let titleWords = [];
-        //       let sanitizedTitle = '';
-        
-        //       // Separate words
-        //       if (data.songtitle.includes('_')) {
-        //         titleWords = data.songtitle.split('_');
-        //       }
-        //       if (data.songtitle.includes(' ')) {
-        //         titleWords = data.songtitle.split(' ');
-        //       }
-        
-        //       // Sanitize word
-        //       titleWords.forEach(word => {
-        //         let sanitizedWord = word[0].toLocaleUpperCase() + word.substring(1).toLocaleLowerCase();
-        //         sanitizedTitle += sanitizedWord + ' ';
-        //       })
-        
-        //       // Set title
-        //       if (mounted) {
-        //         if (sanitizedTitle === "") {
-        //           setSongName("Etikett Radio Archive")
-        //         } else {
-        //         setSongName(sanitizedTitle);
-        //         }
-        //       }
-        //     })
-        //     .catch((err, data) => {
-        //       setSongName("Etikett Radio Archive");
-        //     })
-        // };
-        // getSongName();
-        // const everyFifteenSeconds = setInterval(getSongName, 1000 * 15);
-        // const everySecond = setInterval(timer, 1000)
-        // return () => {
-        //   clearInterval(everyFifteenSeconds);
-        //   clearInterval(everySecond);
-        //   mounted = false;
-        // }
+          }
+        })
+        .catch((err, data) => {
+          setSongName('Etikett Radio Archive');
+        });
+    };
+    getSongName();
+    const everyFifteenSeconds = setInterval(getSongName, 1000 * 15);
+    const everySecond = setInterval(timer, 1000);
+    return () => {
+      clearInterval(everyFifteenSeconds);
+      clearInterval(everySecond);
+      mounted = false;
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [alert])
-    return (
-        <div className="message">
-          <span className="moving-text"> 
-            {/* <div className="current-time">{time}</div>  */}
-            {/* {renderCurrentSongName()}  */}
-            <div>{context.infoBarMessage}</div>
-          </span>
-        </div>
-    )
+  }, [alert]);
+  return (
+    <div className='message'>
+      <span className='moving-text'>
+        <div className='current-time'>{time}</div>
+        {renderCurrentSongName()}
+        <div>{context.infoBarMessage}</div>
+      </span>
+    </div>
+  );
 }
