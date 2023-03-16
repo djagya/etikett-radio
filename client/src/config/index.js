9d9bf5286e18a9a742126320772dfc79fa13ab62
Danil <danil.kabluk@gmail.com>
2023-03-16 11:24:37 +0100

    navbar and footer

---
+++ client/src/config/index.js
@@ -0,0 +1,8 @@
+export const NAVBAR_MENU_ITEMS = [
+  { href: '/schedule', name: 'schedule.' },
+  { href: '/archive', name: 'archive.' },
+  // { href: '/blog', name: 'blog.' },
+  { href: '/shows', name: 'shows.' },
+  { href: '/contact', name: 'contact.' },
+  { href: '/login', name: 'hosts.' },
+];
